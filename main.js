090fe411ea5fda85125824b01541f95f997d78b7
Dami <obayanjud@gmail.com>
2020-04-17 10:19:01 -0600

    receive tokens and sort

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const TOKEN = '2e425b8eadd06a8054e813647cbf9c046eb5ffa5';
+const TOKEN = '4f3fc6b13c50d9cfbaa230dbe759525d1e130812';
 let Buffer = require('buffer/').Buffer
 
 const PORT = 3000;
@@ -7,6 +7,7 @@ const username = 'obayanju';
 let owner = 'expressjs';
 let repo = 'express';
 let jsFiles = [];
+let identTokens = [];
 
 document.addEventListener('DOMContentLoaded', (event) => {
     let processCodeBtn = document.querySelector("#process-code");
@@ -48,22 +49,7 @@ function processCode() {
 
 }
 
-async function sendCodeToInterpreter(code, filePath) {
-    let req_body = {
-        code: code,
-        filePath: filePath
-    }
-    const url = `http://localhost:${PORT}/parse`
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-            'Authorization': `Basic ${new Buffer(username + ':' + TOKEN).toString('base64')}`
-        },
-        body: JSON.stringify(req_body)
-    })
-    const result = await response.json();
-}
+
 
 function clear(el) {
     el.innerHTML = ''
@@ -84,13 +70,14 @@ async function getAllFiles(path) {
 }
 
 function getJSRepos(filesArr) {
-    filesArr.forEach(content => {
+    filesArr.forEach(async content => {
         if (content.type == 'file' && isJSFile(content.name)) {
-            populateHTMLWithGithubURL(content)
+            await populateHTMLWithGithubURL(content)
             jsFiles.push({ path: content.path, html_url: content.html_url });
         }
         else if (content.type == 'dir') {
-            getAllFiles(content.path).then(json => getJSRepos(json))
+            let data = await getAllFiles(content.path)
+            getJSRepos(data)
         }
     });
 }
@@ -126,30 +113,64 @@ async function getContent(item) {
     return result
 }
 
+async function sendCodeToInterpreter(code, filePath) {
+    let req_body = {
+        code: code,
+        filePath: filePath
+    }
+    const url = `http://localhost:${PORT}/parse`
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'Authorization': `Basic ${new Buffer(username + ':' + TOKEN).toString('base64')}`
+        },
+        body: JSON.stringify(req_body)
+    })
+    const result = await response.json();
+    return result
+}
+
+function compare(a, b) {
+    if (a.Literal.toLowerCase() < b.Literal.toLowerCase()) {
+        return -1
+    } else if (a.Literal.toLowerCase() > b.Literal.toLowerCase()) {
+        return 1
+    }
+    return 0
+}
+
 function populateHTMLWithGithubURL(item) {
     let filesEl = document.querySelector('#js-files-section');
     let a = document.createElement('a');
     a.href = item.html_url;
     a.innerHTML = item.path;
     a.onclick = function () { loadCodeIntoDiv(item); return false }
-    parts = item.path.split('')
+    parts = item.path.split('.')
     // ignore minified js
     if (parts[parts.length - 2] != 'min') {
         getContent(item).then(json => {
-            sendCodeToInterpreter(atob(json.content), item.path)
+            sendCodeToInterpreter(atob(json.content), item.path).then(data => {
+                data.forEach(item => {
+                    identTokens.push(item)
+                });
+                identTokens.sort(compare)
+                console.log(identTokens)
+            })
         })
     }
+
     filesEl.appendChild(a);
     filesEl.appendChild(document.createElement('br'));
 }
 
-function parseGithubURL(url) {
+async function parseGithubURL(url) {
     let parts = url.split('/');
     owner = parts[parts.length - 2]
     repo = parts[parts.length - 1]
 
-    getAllFiles('').then(async result => {
-        getJSRepos(result);
+    await getAllFiles('').then(async result => {
+        await getJSRepos(result);
     });
+    console.log("done?", identTokens.length)
 }
-
